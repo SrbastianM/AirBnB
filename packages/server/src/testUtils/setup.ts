30d29b4bcedb326d7fef5651a6c4a8d4821cbc82
Sebastian <sebas907.l@gmail.com>
2022-01-12 22:08:37 -0500

    aññadiendo setup.ts donde hay un error referente a el address, trabajando en su correccion

--- packages/server/src/testUtils/setup.ts
+++ packages/server/src/testUtils/setup.ts
@@ -3,6 +3,6 @@ import { AddressInfo } from "net";
 
 export const setup = async () => {
   const app = await startServer();
-  // const { port } = app.address() as AddressInfo;
-  // process.env.TEST_HOST = `http://127.0.0.1:${port}`;
+  const { port } = app.address() as AddressInfo;
+  process.env.TEST_HOST = `http://127.0.0.1:${port}`;
 };
